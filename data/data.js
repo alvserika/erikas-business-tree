aab98d8734c5a52363df20b5a3c814b6798b883a
Erika Alves <erikaalves.m.silva@gmail.com>
2023-10-31 12:15:02 -0300

    Links finanalizados
    Modularização dos links finalizado

--- data/data.js
+++ data/data.js
@@ -11,4 +11,13 @@ export const botoes = [
         name: 'Minhas Playlists',
         link: 'https://open.spotify.com/user/alveserika?si=4599b54e221e41b6'
     },
+    {
+        name: 'Pinterest',
+        link: 'https://br.pinterest.com/alvserika/_saved/'
+    },
+    {
+        name: 'Fale comigo',
+        link: 'https://wa.me/message/QEUJHVRJ4QNWP1'
+    },
+    
 ]
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ import { Botoes } from "./components/Botoes.js";
 import { botoes } from "./data/data.js";
 export { Botoes }
 
-const botoes = document.querySelector('.links')
-Botoes.map((botao)=> {
-    botoes.innerHTML += Botoes(botao)
+const botoesContainer = document.querySelector('.links');
 
-})
+botoes.forEach((botao) => {
+    botoesContainer.innerHTML += Botoes(botao);
+});
 
 
 
