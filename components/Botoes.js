eea7a8f3bd6c234341fd6259f46803f09a89d990
Erika Alves <erikaalves.m.silva@gmail.com>
2023-11-01 22:59:45 -0300

    Botão personalizado

--- components/Botoes.js
+++ components/Botoes.js
@@ -1,6 +1,6 @@
 export const Botoes = ({name, link}) => {
     return `
-            <div class="d-grid gap-2">
+        <div class="d-grid gap-2">
             <button class="btn btn-outline-dark p-2" type="button"><a href="${link}" target="_blank">${name}</a></button>
         </div>
         `
--- data/data.js
+++ data/data.js
@@ -12,10 +12,6 @@ export const botoes = [
         link: 'https://open.spotify.com/user/alveserika?si=4599b54e221e41b6'
     },
     {
-        name: 'Pinterest',
-        link: 'https://br.pinterest.com/alvserika/_saved/'
-    },
-    {
         name: 'Fale comigo',
         link: 'https://wa.me/message/QEUJHVRJ4QNWP1'
     },
