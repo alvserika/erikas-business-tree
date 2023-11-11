650229fdb5b8573795c8bb706b25a4849a908edb
Erika Alves <erikaalves.m.silva@gmail.com>
2023-11-10 22:54:33 -0300

    SWITCHER

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import { Botoes } from "./components/Botoes.js";
 import { botoes } from "./data/data.js";
 export { Botoes }
 
+
 const botoesContainer = document.querySelector('.links');
 
 botoes.forEach((botao) => {
     botoesContainer.innerHTML += Botoes(botao);
 });
+
+function trocarPosicao() {
+    const botao = document.querySelector('#switcher')
+    botao.style.switch.button.left = inital
+    botao.style.switch.button.right = 0
+
+}
+
+
 
 
 
